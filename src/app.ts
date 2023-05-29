6deca26fce57f97b1166a8f0ab1747235e7983d1
Alfat <alfat.cse.21@gmail.com>
2023-05-29 11:30:48 +0200

    set up

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, Request } from 'express'
 import cors from 'cors'
 const app: Application = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: any) => {
   res.send('Hello World!')
 })
 
